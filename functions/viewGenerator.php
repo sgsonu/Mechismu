3d9d13232aedca9070d9e30f3e5d5835394cd111
Sourav Prasad <souravpd007@gmail.com>
2019-09-29 19:09:01 +0530

    Added Functions

---
+++ functions/viewGenerator.php
@@ -0,0 +1,17 @@
+<?php
+     /* @author         Sourav Prasad
+        @Description    page for generating the views
+     */
+if(isset($_GET['url'])){
+    $page = $_GET['url'];
+    if(file_exists("views/".$page.".php")){
+        require_once("views/".$page.".php");
+    }
+    else{
+        require_once("views/404.php");
+    }
+}
+else{
+    require_once("views/index.php");
+}
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+
+<?php
+    /* @author         Sourav Prasad
+       @Description    Mechismu Page
+     */
+    
+    require_once("views/header.php");
+    require_once("functions/viewGenerator.php");
+    require_once("views/footer.php");
+?>
