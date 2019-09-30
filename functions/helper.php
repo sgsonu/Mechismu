5d6d9058938ec2dd6a3829ae18712ae43a7ed301
Sourav Prasad <souravpd007@gmail.com>
2019-09-30 20:02:12 +0530

    Finished Pages

--- functions/helper.php
+++ functions/helper.php
@@ -1,6 +1,8 @@
 <?php
 require_once("./functions/Navigation.php");
 require_once("./functions/TextBlock.php");
+require_once("./functions/Paragraph.php");
+require_once("./functions/Events.php");
 ?>
 
       
--- views/home.php
+++ views/home.php
@@ -1,121 +1,57 @@
 <div class="container">
     <div class="row mb-3">
         <div class="col">
-            <?php echo TextBlock::GenerateTextBlock("The Coolest Co-Working Space in Prague","Where you choose to work has impact on your productivity and creativity. It
-                      also says a lot about your business. We are the right office space.","","Reserve Your
-                      Spot");?>
+            <?php echo TextBlock::GenerateTextBlock("Mechismu | Racing","A team of dedicated under graduate engineers who put in all their efforts and passion to achieve their one and only goal to make a performance car.","","Are You Excited!");?>
         </div>
     </div>
     <section id="sec-about" class="sec-about pt-5 pb-5">
         <div class="container">
             <div class="row justify-content-center text-center">
-                <div class="col-md-10 col-lg-8">
-                    <h1 class="h4">About us</h1>
-                    <p class="mt-4 mb-4">Co-working spaces are brilliant for smaller companies of up to 4 people who
-                        want a regular workspace. Cost effective, flexible and full of a vibrant energy that comes from
-                        hundreds of like-minded people going it alone.</p>
+                <div class='col-md-10 col-lg-8'>
+                    <?php require_once("./functions/SVG.php");?>
+                    <?php echo Paragraph::GenerateParagraph("Mechismu Racing is a student organization at IIT (ISM) Dhanbad, founded in 2008 with a vision to pioneer development and deployment of innovative technologies in the Indian Automobile Sector. We design, engineer and race our vehicles at SAE Collegiate Design Competitions like the Formula SAE and Baja SAE. Innovation and experimentation are the key drivers in our quest for excellence. Our achievements over the past three years stand testament to our engineering prowess. The team has always operated independently, transferring the knowledge obtained from year to year. ","About Us");?>
                 </div>
             </div>
-             <div class="row mt-4">
-            <div class="col-sm-4">
-                <h4>350</h4>
+            <div class="row mt-4">
+                <div class="col-sm-4">
+                    <h4>35</h4>
 
-                <hr />
+                    <hr />
 
-                <h5>
-                    members</h5>
+                    <h5>
+                        students</h5>
+                </div>
+
+                <div class="col-sm-4">
+                    <h4>8</h4>
+
+                    <hr />
+
+                    <h5>departments</h5>
+                </div>
+
+                <div class="col-sm-4">
+                    <h4>1</h4>
+
+                    <hr />
+
+                    <h5>car</h5>
+                </div>
             </div>
-
-            <div class="col-sm-4">
-                <h4>60</h4>
-
-                <hr />
-
-                <h5>co-working spaces </h5>
-            </div>
-
-            <div class="col-sm-4">
-                <h4>3</h4>
-
-                <hr />
-
-                <h5>members' bars</h5>
-            </div>
-        </div>
             <div class="row mt-4">
                 <div class="gallery-wrap">
                     <div class="item item-1"></div>
-                    <div class="iteḍm item-2"></div>
+                    <div class="item item-2"></div>
                     <div class="item item-3"></div>
                     <div class="item item-4"></div>
                     <div class="item item-5"></div>
                 </div>
             </div>
         </div>
-       
+
 </div>
 </section>
-<div class="timeline-section mb-10">
-            
-    <div class="container-fluid">
-    <div class="container">
-        <h2 class="pb-3 pt-2">Vertical Left-Right Timeline</h2>
-        <!--first section-->
-        <div class="row align-items-center how-it-works">
-        <div class="col-2 text-center bottom">
-            <div class="circle">1</div>
-        </div>
-        <div class="col-6">
-            <h5>Fully Responsive</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor gravida aliquam. Morbi orci urna, iaculis in ligula et, posuere interdum lectus.</p>
-        </div>
-        </div>
-        <!--path between 1-2-->
-        <div class="row timeline">
-        <div class="col-2">
-            <div class="corner top-right"></div>
-        </div>
-        <div class="col-8">
-            <hr/>
-        </div>
-        <div class="col-2">
-            <div class="corner left-bottom"></div>
-        </div>
-        </div>
-        <!--second section-->
-        <div class="row align-items-center justify-content-end how-it-works">
-        <div class="col-6 text-right">
-            <h5>Using Bootstrap</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor gravida aliquam. Morbi orci urna, iaculis in ligula et, posuere interdum lectus.</p>
-        </div>
-        <div class="col-2 text-center full">
-            <div class="circle">2</div>
-        </div>
-        </div>
-        <!--path between 2-3-->
-        <div class="row timeline">
-        <div class="col-2">
-            <div class="corner right-bottom"></div>
-        </div>
-        <div class="col-8">
-            <hr/>
-        </div>
-        <div class="col-2">
-            <div class="corner top-left"></div>
-        </div>
-        </div>
-        <!--third section-->
-        <div class="row align-items-center how-it-works">
-        <div class="col-2 text-center top">
-            <div class="circle">3</div>
-        </div>
-        <div class="col-6">
-            <h5>Now with Pug and Sass</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor gravida aliquam. Morbi orci urna, iaculis in ligula et, posuere interdum lectus.</p>
-        </div>
-        </div>
-    </div>
-    </div>
-</div>
+
+
 </div>
 </div>
